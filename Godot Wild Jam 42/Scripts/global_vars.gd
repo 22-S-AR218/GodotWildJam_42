597e1d71655517ed684e117dcda9189cfa1138e7
Jeffrey Work <28738403+jlgangwisch@users.noreply.github.com>
2022-02-16 13:30:14 -0500

    aoeu

--- Godot Wild Jam 42/Scripts/global_vars.gd
+++ Godot Wild Jam 42/Scripts/global_vars.gd
@@ -1,6 +1,6 @@
 extends Node
 var keys : int = 0
 var current_level : int = 0
-var level0 : PackedScene = preload("res://Levels/Level0.tscn")
-var level1 : PackedScene = preload("res://Levels/Level1.tscn")
-var levels : Array = [level0, level1]
+#var level0 : PackedScene = preload("res://Levels/Level0.tscn")
+#var level1 : PackedScene = preload("res://Levels/Level1.tscn")
+#var levels : Array = [level0, level1]
---
+++ Godot Wild Jam 42/Tutorials.gd
@@ -0,0 +1,4 @@
+#How to Create a Menu in Godot
+#https://www.youtube.com/watch?v=niuItBgRKCo
+
+#
